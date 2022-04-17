a7339dcab57ca1ab84c89f1978d6481b7fb77cca
Oliver Rojas <oliver.rp2003@gmail.com>
2022-04-17 01:04:07 -0500

    avanze de pygr avanzado 1.1

--- AppCpder/urls.py
+++ AppCpder/urls.py
@@ -2,7 +2,7 @@
 from django import views
 from django.urls import path
 #, numero_de_usuario,otra_vista,numero_random, mi_plantilla,probandoTemplate,curso, nuevo_curso, profesores
-from .views import buscar, findCamada, inicio,cursos, estudiantes,profesores
+from .views import buscar, editProf, eliminarProfesor, findCamada, inicio,cursos, estudiantes,profesores,showProfesores
 #, inicio_1
 urlpatterns = [
     # path('',inicio),
@@ -11,23 +11,16 @@ urlpatterns = [
     path('cursos',cursos,name="Cursos"),
     path('profesores',profesores,name="Profesores"),
     path('estudiantes',estudiantes,name="Estudiantes"),
-    # path('entregables',entregables,name="Entregables"),
-    # path('cursoFormulario',cursoFormulario ,name="CursoFormulario"),
-    # path('profesorFormulario', profesorFormulario ,name="profesorFormulario"),
     path('findCamada',findCamada,name="findCamada"),
     path('AppCpder/buscar/',buscar),
-    #creados con el ppt
-    # path('1',inicio_1),
-    # path('cursos',cursos),
-    # path('profesores',profesores),
-    # path('estudiantes',estudiantes),
-    # path('entregables',entregables),
+    #CRUD Básico
+    path('showProfesores',showProfesores,name="showProfesores"),
+    path('eliminarProfesor/<profesor_nombre>/',eliminarProfesor, name="EliminarProfesor"),
+    #comenta ps
+    path('editProfesor/<profesor_nombre>/',editProf, name="EditProfesor"),
+    #CRUD mas simple
     
-    #sacado de lo de Django
-    # path('new-curso',nuevo_curso),
-    # path('new-view',otra_vista),
-    # path('random',numero_random),
-    # path('usuario/<int:numero>',numero_de_usuario),
-    # path('plantilla',mi_plantilla),
-    # path('plantilla-dj2',probandoTemplate),
+    path('showcursos/',views.CursoList.as_view(),name="profesor_list")
+    
+    
 ]
--- AppCpder/views.py
+++ AppCpder/views.py
@@ -1,5 +1,10 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+
 
 from AppCpder.models import Curso, Estudiante, Profesor
 from AppCpder.forms import CursoFormulario, ProfesorFormulario, EstudiantesForm
@@ -39,10 +44,11 @@ def profesores(request):
             informacion = miFormulario.cleaned_data
             
             new_profesor = Profesor(nombre=informacion['nombre'],apellido=informacion['apellido'],
-                             email=informacion['email'],profesion=informacion['profesion'])
+                email=informacion['email'],profesion=informacion['profesion'])
             
             new_profesor.save()
-            return render(request,"AppCpder/inicio.html",{'profesor':new_profesor}) #Vuelvo al inicio o a donde quieran
+            #return render(request,"AppCpder/inicio.html",{'profesor':new_profesor}) #Vuelvo al inicio o a donde quieran
+            return render(request,"AppCpder/inicio.html") #Vuelvo al inicio o a donde quieran
     
     else:
         miFormulario=ProfesorFormulario()#Formulario vacio para construir el html
@@ -89,4 +95,86 @@ def buscar(request):
         respuesta="No enviaste datos"
     
     return render(request, "AppCpder/inicio.html", {"respuesta":respuesta})
+
+
+
+def showProfesores(request):
+    profesores = Profesor.objects.all() #trae todos los profesores creados
+    contexto={"profesores":profesores}
+    
+    return render(request, "AppCpder/leerProfesores.html",contexto)
+
+
+def eliminarProfesor(request, profesor_nombre):
+    
+    profesor = Profesor.objects.get(nombre=profesor_nombre)
+    profesor.delete()
+    
+    #vuelvo al menú
+    # profesores= Profesor.objects.all()
+    #return render(request,"AppCpder/leerProfesores.html",{"profesores": profesores})
+    #vuelvo al menú más rápido y muestro a los profesores
+    return redirect('showProfesores')
+
+# def leerProfesores(request, profesor_nombre='a'):
+    
+#     if profesor_nombre!='a':
+#         profesor = Profesor.objects.get(nombre=profesor_nombre)
+#         profesor.delete()
+    
+#     profesores = Profesor.objects.all() #trae todos los profesores creados
+#     contexto={"profesores":profesores}
+    
+#     return render(request, "AppCpder/leerProfesores.html",contexto)
+
+def editProf(request, profesor_nombre):
+    
+    #Recibe el nombre del profesor a modificar
+    prof= Profesor.objects.get(nombre=profesor_nombre)
+    
+    #Si es metodo POST hago lo mismo que el agregar
+    if request.method=='POST':
+        miFormulario = ProfesorFormulario(request.POST)
+        print(miFormulario)
+        
+        if miFormulario.is_valid: #Si pasó la validación de Django
+            info= miFormulario.cleaned_data
+            prof.nombre = info['nombre']
+            prof.apellido = info['apellido']
+            prof.email = info['email']
+            prof.profesion = info['profesion']
+            
+            prof.save()
+        return render(request, "AppCpder/inicio.html")
+    #En caso no sea post (osea GET, lo que quiere decir que es la primera ve que se entra a editar)
+    else:
+        miFormulario= ProfesorFormulario(initial={'nombre': prof.nombre, 
+            'apellido':prof.apellido, 'email':prof.email, 'profesion': prof.profesion})
+        #Voy al html que me permite editar
+        return render(request,"AppCpder/editProfesor.html",{"miFormulario":miFormulario, "profesor_nombre":profesor_nombre})
+
+#CRUD MAS SIMPLE
+
+class CursoList(ListView):
+            
+    model= Curso
+    template_name = "AppCpder/cursos_list.html"
+
+class CursoDetalle(DetailView):
+    model=Curso
+    template_name ="AppCpder/curso_detail.html"
+
+class CursoCreacion(CreateView):
+    model=Curso
+    succes_url = "/AppCpder/curso/list"
+    fields = ['nombre','camada']
+
+class CursoUptate(UpdateView):
+    model=Curso
+    succes_url = "/AppCpder/curso/list"
+    fields = ['nombre','camada']
+    
+class CursoDelete(DeleteView):
+    model=Curso
+    succes_url = "/AppCpder/curso/list"
     
